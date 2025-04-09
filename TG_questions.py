1f53f9857bdd9330d2724b8f9ab591f32f3e8686
TualGroix <tual.groix@eleve.ensai.fr>
2025-04-09 17:10:17 +0200

    Update TG_questions.py

--- TG_questions.py
+++ TG_questions.py
@@ -3,6 +3,8 @@ import pandas as pd
 # Lire le fichier CSV
 donnees = pd.read_csv("athlete_events.csv")
 
+# question : quel sportif a participé au plus d'épreuves sans jamais être médaillé ?
+
 # Filtrer les données pour garder uniquement les athletes avec médailles
 donnees2 = donnees.dropna(subset=["Medal"])  # supprimer les valeurs manquantes
 donnees2 = donnees2[donnees2["Medal"] != "NA"]  # Exclure les entrées "NA"
@@ -14,5 +16,15 @@ donnees3 = donnees3[donnees3["Medal"] != "Bronze"]  # Exclure les entrées "Bron
 
 # conserver uniquement les athletes qui n'ont jamais eu de médaille
 donnees4 = pd.merge(
-    donnees3, donnees2, how='outer', on='ID',indicator=True).query(
+    donnees3, donnees2, how='outer', on='ID', indicator=True).query(
         '_merge=="left_only"').drop(columns='_merge')
+
+print(donnees4)
+
+# trier les ID par nombre de participations
+
+
+compte = donnees4['ID'].value_counts(dropna=False).reset_index()
+print(compte)
+compte.to_csv('compte.csv', sep=';', encoding='utf-8', index=False)
+donnees4.to_csv('donnees4.csv', sep=';', encoding='utf-8', index=False)
