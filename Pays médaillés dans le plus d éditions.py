c46c32e93e2080592e04a72afd37cdc5dc2d5b13
Gabriel Lecointre <gabriel.lecointre@eleve.ensai.fr>
2025-04-22 17:19:41 +0200

    màj 3e programme

--- Pays médaillés dans le plus d éditions.py
+++ Pays médaillés dans le plus d éditions.py
@@ -8,8 +8,7 @@ pandas.set_option("display.max_rows", 150)
 
 # Lire le fichier .csv avec la base de données (CHEMIN DU FICHIER)
 BDJO = pandas.read_csv(os.path.join("donnees_jeux_olympiques", "athlete_events.csv"))
-
-# Comment la taille des sportifs varient-ils selon les sports et le sexe ?
+pays = pandas.read_csv(os.path.join("donnees_jeux_olympiques", "noc_regions.csv"))
 
 BDJOpaysmedailles = BDJO.dropna(subset=["Medal"])
 # Je supprime les lignes sans médailles. Ne reste plus qu’or, argent et bronze.
@@ -53,15 +52,61 @@ pays_ete = (
     .reset_index(name="Nb_JO_E")
 )
 
-# fillna
 # set_index
-# astype(int)
 
 # Fusionner les trois DataFrames pour avoir un tableau complet
 paysmedailles = pandas.merge(pays_total, pays_ete, on="NOC", how="left")
 paysmedailles = pandas.merge(paysmedailles, pays_hiver, on="NOC", how="left")
+# jointure
+paysmedailles = pandas.merge(
+    paysmedailles,
+    pays[["NOC", "region"]],
+    on="NOC",
+    how="left",
+)
+
+# Le tableau des pays est imparfait soit 4 corrections à la main :
+# SGP = Singapore (manquant)
+# HKG = Hong Kong à affecter à Hong Kong et non à China (2 délégations différentes)
+# WIF = Indes occidentales à affecter à Jamaica et non à Trinidad (plus logique car
+# plus peuplée et les médailles sont remportées par des Jamaïquains)
+# GDR = Allemagne de l’Est affecté à East Germany (plus logique car période où
+# Allemagne de l’Ouest a aussi sa délégation)
+
+corrections = {
+    "SGP": "Singapore",
+    "HKG": "Hong Kong",
+    "WIF": "Jamaica",
+    "GDR": "East Germany",
+}
+
+paysmedailles["region"] = (
+    paysmedailles["NOC"].map(corrections).fillna(paysmedailles["region"])
+)
+
+# On remplace les NaN par des zéros.
+paysmedailles.fillna(0, inplace=True)
+
+# On transforme les flottants en entiers.
+paysmedailles[["Nb_JO", "Nb_JO_H", "Nb_JO_E"]] = paysmedailles[
+    ["Nb_JO", "Nb_JO_H", "Nb_JO_E"]
+].astype(int)
+
 paysmedailles_tri = paysmedailles.sort_values(by="Nb_JO", ascending=False)
 
 # Afficher le résultat
 print(paysmedailles_tri)
 print(paysmedailles_tri.columns)
+
+# Les pays suivants (ou leur prolongement historique) ont eu des médailles sous des
+# codes différents selon les périodes, le nom de pays renvoyé par le tableau est
+# identique et les données peuvent être sommées :
+# ANZ = Australasie (soit Australie + Nouvelle-Zélande) affecté à Australia
+# BOH et TCH = Bohême et Tchécoslovaquie (soit République tchèque et Slovaquie)
+# affecté à Czech Republic
+# YUG et SCG = Yougoslavie (soit 7 pays) et Serbie-et-Monténégro (soit Serbie et
+# Monténégro) affecté à Serbia
+# URS et EUN = Union soviétique (soit 15 pays) et Équipe unifiée (12 pays)
+# affecté à Russia
+# FRG = Allemagne de l’Ouest affecté à Germany
+# WIF = Indes occidentales affecté à Jamaica
