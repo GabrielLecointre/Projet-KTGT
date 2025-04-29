3b8069e7792ed992fed1603832047fe6c45e6bbb
Teodora Moldovan <teodora.moldovan@eleve.ensai.fr>
2025-04-29 14:19:42 +0200

    programme question  2

--- Q2_Teo_pandas.py
+++ Q2_Teo_pandas.py
@@ -27,7 +27,7 @@ print(donnees_jo.columns.tolist())
 
 
 def correct_collective_medals(df):
-    collective_events = df.groupby(["Games", "Event", "Team"])["ID"].nunique()
+    collective_events = df.groupby(["Year", "Event", "NOC"])["ID"].nunique()
     collective_events = (
         collective_events[collective_events > 1]
         .index.get_level_values("Event")
@@ -46,7 +46,7 @@ def correct_collective_medals(df):
             return group
 
     corrected_collective = (
-        df_collective.groupby(["Games", "Event"])
+        df_collective.groupby(["Year", "Event"])
         .apply(process_collective)
         .reset_index(drop=True)
     )
@@ -54,24 +54,12 @@ def correct_collective_medals(df):
     return pd.concat([corrected_collective, df_individual], ignore_index=True)
 
 
-# Assuming your DataFrame is named 'olympics_data'
-olympics_corrected = correct_collective_medals(donnees_jo.copy())
+jo_correctes = correct_collective_medals(donnees_jo.copy())
 
 
-def classement_jo(donnees, annee, nb_pays=15):
-    """
-    Affiche le classement des pays aux JO selon les médzailles obtenues
-
-    Paramètres:
-    - donnees: la table des JO
-    - annee: l'année des JO qu l'on souhaite afficher
-    - nb_pays: Nombre de pays à lister dans le classement (par défaut: 15)
-
-    Retourne:
-    - DataFrame contenant le classement des pays
-    """
+def classement_jo(donnees, annee, nb_pays=15, fichier_excel=None):
     # permet de filtrer les données pour garder uniquement l'année spécifiée
-    jo_annee = donnees[donnees["Year"] == annee]
+    jo_annee = donnees[(donnees["Year"] == annee) & (donnees["Season"] == "Summer")]
     jo_medailles = jo_annee[jo_annee["Medal"].notnull()]
 
     # Créer une table pivot pour compter les médailles par type
@@ -100,10 +88,22 @@ def classement_jo(donnees, annee, nb_pays=15):
     # Trier UNIQUEMENT par médailles d'or
     classement = medailles_par_pays.sort_values(by="Gold", ascending=False)
 
-    return classement.head(nb_pays)
+    # Récupérer les n meilleurs pays
+    top_classement = classement.head(nb_pays)
+
+    # Exporter vers un fichier Excel si un nom de fichier est donné
+    if fichier_excel:
+        top_classement.to_excel(fichier_excel)
+
+    return top_classement
 
 
-# Exemple d'utilisation
-classement_1984 = classement_jo(donnees_jo, 1984)
+# Utilisation : classement + exportation Excel
+classement_1984 = classement_jo(
+    jo_correctes, 1984, fichier_excel="classement_jo_1984.xlsx"
+)
+
+
+classement_1984 = classement_jo(jo_correctes, 1984)
 print("Classement des pays aux JO de 1984 :")
 print(f"{classement_1984}")
